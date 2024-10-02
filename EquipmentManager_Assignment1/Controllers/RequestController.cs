aa1edb19f00d2391bd933345d7a54e0cccaf8f8f
aanjaneyagupta <gupta.aanjaneya@gmail.com>
2024-10-02 12:09:44 +0530

    Request Form Page Completed

---
+++ EquipmentManager_Assignment1/Controllers/RequestController.cs
@@ -0,0 +1,46 @@
+﻿using EquipmentManager_Assignment1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EquipmentManager_Assignment1.Controllers
+{
+    public class RequestController:Controller
+    {
+        private static List<Request> requests = new List<Request>();
+        private static int id = 1;
+
+        [HttpGet]
+        public IActionResult RequestForm()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult RequestForm(Request request)
+        //{
+
+        //}
+        [HttpPost]
+        public IActionResult RequestForm(Request request)
+        {
+            if (ModelState.IsValid)
+            {
+                request.Id = id++;
+                requests.Add(request);
+                return RedirectToAction("Confirmation");
+            }
+            return View(request);
+        }
+
+        // GET: /Confirmation
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+
+        // GET: /Requests (for Admin view)
+        public IActionResult Requests()
+        {
+            return View(requests);
+        }
+    }
+}
---
+++ EquipmentManager_Assignment1/Models/Request.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EquipmentManager_Assignment1.Models
+{
+    public enum EquipmentType { Laptop, Phone, Tablet, Other}
+    public enum RoleType { Student, Professor}
+    public class Request
+    {
+        public int? Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required"), EmailAddress]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required"), Phone]
+        public string? PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Role is required")]
+        public RoleType? Role {  get; set; }
+
+        [Required(ErrorMessage = "Equipment Type is required")]
+        public EquipmentType? Type { get; set; }
+
+        [Required(ErrorMessage = "Request Details are required")]
+        public string? RequestDetails {  get; set; }
+
+        [Required(ErrorMessage = "Duration is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Enter a positive number")]
+        public int? Duration { get; set; }
+    }
+}
