2af7590b290e19d5bb466939954adc3f7a152253
aanjaneyagupta <gupta.aanjaneya@gmail.com>
2024-10-02 12:12:49 +0530

    Added regex to the phone number

--- EquipmentManager_Assignment1/Models/Request.cs
+++ EquipmentManager_Assignment1/Models/Request.cs
@@ -15,6 +15,7 @@ namespace EquipmentManager_Assignment1.Models
         public string? Email { get; set; }
 
         [Required(ErrorMessage = "Phone is required"), Phone]
+        [RegularExpression(@"\d{3}-\d{3}-\d{4}",ErrorMessage =("Please enter the phone number in correct format."))]
         public string? PhoneNumber { get; set; }
 
         [Required(ErrorMessage = "Role is required")]
